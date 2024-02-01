d469adedfaee6511c83d8e36f1a430553bc9d044
Yauhen Habinet <yauhen_habinet@epam.com>
2024-02-01 22:27:14 +0300

    Home task number Three provided

---
+++ homeTaskThree/task.js
@@ -0,0 +1,54 @@
+// Task one: Add of dirrefent types
+//    a) String + boolean
+let add1 = 'Test string' + true;
+console.log(add1);
+
+//    b) String + number
+let add2 = 'Test string' + 4;
+console.log(add2);
+
+//    c) number + boolean
+let add3 = 4 + true;
+console.log(add3);
+
+// Task two: Multiply different types
+//    a) string * boolean
+let mul1 = 'Test String' * true;
+console.log(mul1);
+
+//    b) string * number
+let mul2 = 'Test Strung' * 4;
+console.log(mul2);
+
+//    c) number * boolean
+let mul3 = 4 * false;
+console.log(mul3);
+
+//Task three: Devide different types
+//    a) string / boolean
+let dev1 = 'Test string' / true;
+console.log(dev1);
+
+//    b) string / number
+let dev2 = 'Test string' / 5;
+console.log(dev2);
+
+//    c) number / boolean
+let dev3 = 3 / true;
+console.log(dev3);
+
+// Task four: Data conversion
+//    a) Number
+
+let num1 = parseInt('1234');
+console.log(num1);
+
+//    b) String
+
+let str1 = String(true);
+console.log(str1);
+
+//    c) boolean
+
+let  bol1 = Boolean('');
+console.log(bol1);
