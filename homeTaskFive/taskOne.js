dd01bbabade46f9a39377c7a9c60a4b9a20b075b
yauhenFan <157898759+yauhenFan@users.noreply.github.com>
2024-02-11 18:48:44 +0300

    Update taskOne.js

--- homeTaskFive/taskOne.js
+++ homeTaskFive/taskOne.js
@@ -4,10 +4,11 @@ let yearStart = startDate.getFullYear();
 
 function caclFriday(year) {
     let yearEnd = currDate.getFullYear();
+    let startMonth = startDate.getMonth();
     let currMonth = currDate.getMonth();
     let frayArr = [];
     let monthSum = (yearEnd - yearStart) * 12 - currMonth;
-     for (let month = 0; month < monthSum; month++)  {
+     for (let month = startMonth; month < monthSum; month++)  {
         let d = new Date(year, month, 13);
         if(d.getDay() === 5) {
           frayArr.push(d);
