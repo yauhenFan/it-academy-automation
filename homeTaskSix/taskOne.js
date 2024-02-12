b50abf862f4d258abe33bb5f67b573637348c45f
Yauhen Habinet <yauhen_habinet@epam.com>
2024-02-12 22:56:11 +0300

    Solution for home task five

--- homeTaskSix/taskOne.js
+++ homeTaskSix/taskOne.js
@@ -2,9 +2,11 @@ function newFunc(N, M) {
 let arr = [0, 1];
 let el = 0;
 
-for (let i = N; i <= M; i++) {
- el = i <= 1 ? i : arr[i-1] + arr [i-2];
+for (let i = 2; i <= M; i++) {
+ el = i <= 1 ? i : arr[i-1] + arr[i-2];
  arr.push(el);
 }
- return arr;
+ return arr.slice(N);
 }
+
+console.log(newFunc(1, 8));
